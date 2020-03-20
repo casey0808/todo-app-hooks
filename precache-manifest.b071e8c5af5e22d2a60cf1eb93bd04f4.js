46dde983dd9dbf6ce522e9a08ac9b29ab0116e90
casey0808 <casey_c0808@outlook.com>
2020-03-20 11:57:27 +0800

    Updates

---
+++ precache-manifest.b071e8c5af5e22d2a60cf1eb93bd04f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97509894e84c6aace5e09e0cb6a78a5b",
+    "url": "/todo-app-hooks/index.html"
+  },
+  {
+    "revision": "e197b156c70cfee7cc45",
+    "url": "/todo-app-hooks/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "4421025935e47324fcd8",
+    "url": "/todo-app-hooks/static/js/2.02a1530c.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/todo-app-hooks/static/js/2.02a1530c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e197b156c70cfee7cc45",
+    "url": "/todo-app-hooks/static/js/main.65b69ee2.chunk.js"
+  },
+  {
+    "revision": "c3ceb213dfc60cee3a6c",
+    "url": "/todo-app-hooks/static/js/runtime-main.dc0e55f8.js"
+  }
+]);
